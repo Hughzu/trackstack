7f2f818f95bbd796b6f4d7a9ece9476d03fffd48
Hughes Stiernon <hughesstiernon@gmail.com>
2026-02-04 15:33:13 +0100

    add ssr

--- src/web/astro.config.mjs
+++ src/web/astro.config.mjs
@@ -2,9 +2,14 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import AstroPWA from "@vite-pwa/astro";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  }),
   integrations: [
     AstroPWA({
       registerType: "autoUpdate",
