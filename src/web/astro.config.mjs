c71f19a13904646903518a49f54fef10ac3a4453
Hughes Stiernon <hughesstiernon@gmail.com>
2026-02-17 17:34:46 +0100

    fix astro mode

--- src/web/astro.config.mjs
+++ src/web/astro.config.mjs
@@ -9,7 +9,7 @@ import node from "@astrojs/node";
 export default defineConfig({
   output: "server",
   adapter: node({
-    mode: "standalone"
+    mode: "middleware"
   }),
   integrations: [
     AstroPWA({
