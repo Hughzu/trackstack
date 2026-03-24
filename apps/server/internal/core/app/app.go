d79a8c2456ab9e9ebc338f8d31b44661d007d47c
Hughes Stiernon <hughesstiernon@gmail.com>
2026-03-24 12:52:02 +0100

    remove old server

--- apps/server/internal/core/app/app.go
+++
@@ -1,9 +0,0 @@
-package app
-
-import bootstrap "github.com/Hughzu/trackstack/apps/server/internal/app/bootstrap"
-
-type Runtime = bootstrap.Runtime
-
-func NewRuntime() (*Runtime, error) {
-	return bootstrap.NewRuntime()
-}
--- apps/server/internal/modules/users/ports.go
+++
@@ -1,8 +0,0 @@
-package users
-
-import "context"
-
-type UsersStore interface {
-	FindByEmail(ctx context.Context, email string) (User, error)
-	UpdateLastLogin(ctx context.Context, userID string, lastLoginAt string) error
-}
